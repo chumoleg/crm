05d7025c8cbb7cd741d573b616ed65067996a45f
chumoleg <chumoleg@yandex.ru>
2016-08-28 11:18:46 +0700

    change configs

--- environments/prod/common/config/main-web-local.php
+++ environments/prod/common/config/main-web-local.php
@@ -1,16 +1,3 @@
 <?php
-
 return [
-    'components' => [
-        'user'    => [
-            'identityCookie' => [
-                'domain' => '.crm.local',
-            ],
-        ],
-        'session' => [
-            'cookieParams' => [
-                'domain' => '.crm.local'
-            ],
-        ],
-    ],
 ];
