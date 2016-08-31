409065f51a8e52e8d0ba4baac11f0b033780069f
chumoleg <chumoleg@yandex.ru>
2016-08-31 20:52:49 +0700

    change language

--- common/config/main.php
+++ common/config/main.php
@@ -3,13 +3,15 @@
 require(__DIR__ . DIRECTORY_SEPARATOR . 'container.php');
 
 return [
-    'vendorPath'   => dirname(dirname(__DIR__)) . '/vendor',
-    'components'   => [
-        'db'            => [
+    'vendorPath'     => dirname(dirname(__DIR__)) . '/vendor',
+    'language'       => 'ru-RU',
+    'sourceLanguage' => 'en-US',
+    'components'     => [
+        'db'          => [
             'class'   => 'yii\db\Connection',
             'charset' => 'utf8',
         ],
-        'cache'         => [
+        'cache'       => [
             'class' => 'yii\redis\Cache',
             'redis' => [
                 'hostname' => 'localhost',
@@ -17,18 +19,18 @@ return [
                 'database' => 0,
             ]
         ],
-        'curl'          => [
+        'curl'        => [
             'class' => 'linslin\yii2\curl\Curl',
         ],
-        'mailer'        => [
+        'mailer'      => [
             'class'    => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
         ],
-        'authManager'   => [
+        'authManager' => [
             'class' => 'yii\rbac\DbManager',
             'cache' => 'cache',
         ],
-        'log'           => [
+        'log'         => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets'    => [
                 [
