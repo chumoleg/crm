419ca02d0daab64b246a8a60eb34762096f08347
chumakov.o <chumakov.o@thor-dev.ru>
2017-02-02 08:26:22 +0700

    create mail sending

--- common/mail/order/overdueOnToday.php
+++ common/mail/order/overdueOnToday.php
@@ -2,8 +2,13 @@
 use yii\helpers\Html;
 
 echo Html::tag('h4', 'На сегодня просрочено сделок: ' . count($orderData));
+?>
 
-foreach ($orderData as $order) {
-    $orderId = $order['id'];
-    echo Html::a($orderId, 'http://call.crm.local/order/order/view/' . $orderId) . '<br />';
-}
+<table>
+    <?php foreach ($orderData as $order) : ?>
+        <tr>
+            <?php $orderId = $order['id']; ?>
+            <td><?= Html::a($orderId, 'http://call.crm.local/order/order/view/' . $orderId); ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
--- common/mail/order/postponedOnToday.php
+++ common/mail/order/postponedOnToday.php
@@ -7,9 +7,10 @@ echo Html::tag('h4', 'На сегодня отложено сделок: ' . cou
 
 <table>
     <?php foreach ($orderData as $order) : ?>
-        <?php $orderId = $order['id']; ?>
-        <td><?= $orderId; ?></td>
-        <td><?= ArrayHelper::getValue($order, 'companyCustomer.currentOperator.fio'); ?></td>
-        <td><?= Html::a($orderId, 'http://call.crm.local/order/order/view/' . $orderId); ?></td>
+        <tr>
+            <?php $orderId = $order['id']; ?>
+            <td><?= Html::a($orderId, 'http://call.crm.local/order/order/view/' . $orderId); ?></td>
+            <td><?= ArrayHelper::getValue($order, 'companyCustomer.currentOperator.fio'); ?></td>
+        </tr>
     <?php endforeach; ?>
 </table>
